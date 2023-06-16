1f1256f6034b3a6875d0f8b21bd1a0765a457f7d
sophia-bergamo <sophiabergamo7@gmail.com>
2023-06-16 18:10:15 -0300

    connected-database

---
+++ User/src/index.ts
@@ -0,0 +1,18 @@
+import { AppDataSource } from "./data-source"
+import { User } from "./entity/User"
+
+AppDataSource.initialize().then(async () => {
+
+    console.log("Inserting a new user into the database...")
+    const user = new User()
+    user.firstName = "Sophia"
+    user.lastName = "Bergamo"
+    user.age = 18
+    await AppDataSource.manager.save(user)
+    console.log("Saved a new user with id: " + user.id)
+
+    console.log("Loading users from the database...")
+    const users = await AppDataSource.manager.find(User)
+    console.log("Loaded users: ", users)
+
+}).catch(error => console.log(error))
