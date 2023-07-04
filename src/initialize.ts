acb4e671967601e5e4a5fd75d6be8082005114c9
sophia-bergamo <sophiabergamo7@gmail.com>
2023-07-04 12:19:40 -0300

    mutation login

--- src/initialize.ts
+++ src/initialize.ts
@@ -2,87 +2,9 @@ import 'reflect-metadata';
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 import { AppDataSource } from './data-source';
-import { User } from './entity/User';
-import * as bcrypt from 'bcrypt';
 import { unwrapResolverError } from '@apollo/server/errors';
-import { InputErrors } from './test/error';
-
-//GraphQl Schema
-//para colocar input (input NomeInput)
-const typeDefs = `#graphql
-
-  type Query{
-    hello: String
-  }
-
-  #input obrigatório !
-  type Mutation{
-    createUser(input: CreateUserInput!): User
-}
-  
-  input CreateUserInput{
-    name: String!
-    email: String!
-    password: String!
-    birthDate: String!
-  }
-
-   type User {
-    id: ID
-    name: String
-    email: String
-    birthDate: String
-  }
-`;
-//interface para conectar args com os inputs
-//typescript como default é obrigatório, para ser "opcional" -> ?
-interface CreateUserInput {
-  input: {
-    name: string;
-    email: string;
-    password: string;
-    birthDate: string;
-  };
-}
-
-const resolvers = {
-  Query: {
-    hello: () => 'Hello World',
-  },
-  Mutation: {
-    createUser: async (_: any, args: CreateUserInput) => {
-      //busca do email - busca no banco se já existe um email igual
-      const storageUsers = await AppDataSource.manager.findOne(User, { where: { email: args.input.email } });
-      if (storageUsers) {
-        throw new InputErrors('Email já registrado');
-      }
-      //validação da senha - 6 caracteres, com 1 letra e 1 digito
-      const validPassword = args.input.password;
-      const onlyCharacters = /\d/;
-      const onlyNumbers = /[a-zA-Z]/;
-      if (validPassword.length < 6 || !onlyCharacters.test(validPassword) || !onlyNumbers.test(validPassword)) {
-        throw new InputErrors('Senha inválida, deve conter ao menos 6 caracteres, 1 letra e um dígito');
-      }
-      //criação do hash da senha
-      const hashedPassword = await bcrypt.hash(args.input.password, 10);
-
-      //validação do email
-      const validateEmail = args.input.email;
-      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      if (!emailRegex.test(validateEmail)) {
-        throw new InputErrors('Email inválido');
-      }
-
-      const user = new User();
-      user.name = args.input.name;
-      user.email = args.input.email;
-      user.password = hashedPassword; //armazena o hash invés da senha
-      user.birthDate = args.input.birthDate;
-
-      return AppDataSource.manager.save(user);
-    },
-  },
-};
+import { resolvers } from './resolvers';
+import { typeDefs } from './schema';
 
 //Para lidar com Promises, definir a ordem usando o await
 export async function initialize() {
---
+++ src/schema.ts
@@ -0,0 +1,56 @@
+//GraphQl Schema
+//para colocar input (input NomeInput)
+export const typeDefs = `#graphql
+
+  type Query{
+    hello: String
+  }
+
+  #input obrigatório !
+  type Mutation{
+    createUser(input: CreateUserInput!): User
+    login(input: LoginInput!): Login
+}
+  
+  input CreateUserInput{
+    name: String!
+    email: String!
+    password: String!
+    birthDate: String!
+  }
+
+  type Login{
+    token: String!
+    user: User!
+  }
+
+   type User {
+    id: ID
+    name: String
+    email: String
+    birthDate: String
+  }
+
+  input LoginInput {
+      email: String!
+      password: String!
+  }
+`;
+
+//interface para conectar args com os inputs
+//typescript como default é obrigatório, para ser "opcional" -> ?
+export interface CreateUserInput {
+  input: {
+    name: string;
+    email: string;
+    password: string;
+    birthDate: string;
+  };
+}
+
+export interface LoginInput {
+  input: {
+    email: string;
+    password: string;
+  };
+}
