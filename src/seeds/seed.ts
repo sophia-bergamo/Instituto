07423f2163e35c28791f672cc06b8fd35abb3253
sophia-bergamo <sophiabergamo7@gmail.com>
2023-07-26 14:54:42 -0300

    relation database

--- src/seeds/seed.ts
+++ src/seeds/seed.ts
@@ -1,10 +1,10 @@
 import { AppDataSource } from '../data-source';
-import { createFakerUser } from './factory';
+import { createFakerUsers } from './factory';
 
 //executa o DataSource antes de executar a função que cria os usuários
 async function seed() {
   await AppDataSource.initialize();
-  await createFakerUser(50);
+  await createFakerUsers(50);
 }
 
 seed();
